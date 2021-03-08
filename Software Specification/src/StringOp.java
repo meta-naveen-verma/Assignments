81457a7b751723c48637a9431d45ac433dd83534
meta-naveen-verma <79202706+meta-naveen-verma@users.noreply.github.com>
2021-03-08 12:25:01 +0530

    Update StringOp.java

--- Software Specification/src/StringOp.java
+++ Software Specification/src/StringOp.java
@@ -1,42 +1,98 @@
+/*********************************************************************
+ * Name 	:- StringOp
+ * Date 	:- 25-02-2021
+ * Author 	:- Naveen Verma
+ * Purpose 	:- To perform operations(compare, reverse, caseChange, findLargestWord) on string
+ *********************************************************************/
+
 import java.util.*;
 public class StringOp{
-	public static int Compare(String a,String b){
-		if(a.equals(b))
-			return 1;
+	
+	/*****************************************************************************
+	 * Name	:- compare
+	 * Date :- 25-02-2021
+	 * Author :-Naveen Verma
+	 * Purpose :- TO check given strings are equals or not
+	 * Input :- Take two strings
+	 * @param str1
+	 * @param str2
+	 * Output :- gives 1 if str1 and str2 are equal else 0
+	 * @return int value (0 or 1).
+	 *****************************************************************************/
+	public static int compare(String str1,String str2){
+		if(str1.length()==str2.length()){
+			for(int i=0;i<str1.length();i++){
+				if(str1.charAt(i)!=str2.charAt(i)){
+					return 0;
+				}
+			}
+		}
 		else
 			return 0;
+		return 1;
 	}
-	public static String Reverse(String a){
-		String b="";
-		for(int i=a.length()-1;i>=0;i--){
-			b+=a.charAt(i);
+	
+	/*****************************************************************************
+	 * Name	:- reverse
+	 * Date :- 25-02-2021
+	 * Author :-Naveen Verma
+	 * Purpose :- TO reverse the given string
+	 * Input :- Take a strings
+	 * @param str
+	 * Output :- gives reverse of given string str
+	 * @return String result 
+	 *****************************************************************************/
+	public static String reverse(String str){
+		String result="";
+		for(int i=str.length()-1;i>=0;i--){
+			result+=str.charAt(i);
 		}
-		return b;
+		return result;
 	}
+	
+	/*****************************************************************************
+	 * Name	:- caseChange
+	 * Date :- 25-02-2021
+	 * Author :-Naveen Verma
+	 * Purpose :- TO change the case of given string
+	 * Input :- Take a strings
+	 * @param str
+	 * Output :- gives case changed string(upper to lower and lower to upper)
+	 * @return String result 
+	 *****************************************************************************/
+	public static String caseChange(String str){
+		String result="";
+		for(int i=0;i<str.length();i++){
+			if(Character.isLowerCase(str.charAt(i)))
 
-	public static String Casechange(String a){
-		String b="";
-		for(int i=0;i<a.length();i++){
-			if(Character.isLowerCase(a.charAt(i)))
-
-				b+=Character.toUpperCase(a.charAt(i));
+				result+=Character.toUpperCase(str.charAt(i));
 			else
-				b+=Character.toLowerCase(a.charAt(i));
+				result+=Character.toLowerCase(str.charAt(i));
 
 		}
-		return b;
+		return result;
 	}
-
-	public static String largestWord(String a){  
+	
+	/*****************************************************************************
+	 * Name	:- findLargestWord
+	 * Date :- 25-02-2021
+	 * Author :-Naveen Verma
+	 * Purpose :- TO find largest word in given string
+	 * Input :- Take a strings
+	 * @param str
+	 * Output :- gives largest word as a string
+	 * @return String large 
+	 *****************************************************************************/
+	public static String findLargestWord(String str){  
 		String word = "", large="";    
 		String[] words = new String[100];    
 		int length = 0;      
-		a = a + " ";    
+		str+=" ";    
           
-		for(int i = 0; i < a.length(); i++){    
+		for(int i = 0; i < str.length(); i++){    
             
-          if(a.charAt(i) != ' '){    
-              word = word + a.charAt(i);    
+          if(str.charAt(i) != ' '){    
+              word = word + str.charAt(i);    
           }    
           else{    
                 
@@ -47,13 +103,19 @@ public class StringOp{
       }            
  
       large = words[0];    
-   
       for(int k = 0; k < length; k++){      
-          if(large.length() < words[k].length())    
-              large = words[k];    
+          if(large.length() <= words[k].length())    
+              large = words[k]; 
       }    
       return large;
 	}
+	
+	/*****************************************************************************
+	 * Name	:- main 
+	 * Date :- 25-02-2021
+	 * Author :-Naveen Verma
+	 * Purpose :- TO run all the functions
+	 *****************************************************************************/
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Press 1 to compare two string");
@@ -64,21 +126,22 @@ public class StringOp{
 		int ch=sc.nextInt();
 		switch(ch){
 		case 1:
-			String a=sc.next();
-			String b=sc.next();
-			System.out.println(Compare(a,b));
+			String str1=sc.next();
+			String str2=sc.next();
+			System.out.println(compare(str1,str2));
 			break;
 		case 2:
-			String c=sc.next();
-			System.out.println(Reverse(c));
+			String str=sc.next();
+			System.out.println(reverse(str));
 			break;
 		case 3 :
-			String d=sc.next();
-			System.out.println(Casechange(d));
+			String strInCase=sc.next();
+			System.out.println(caseChange(strInCase));
 			break;
 		case 4:
-			String e=sc.next();
-			System.out.println(largestWord(e));
+			sc.nextLine();
+			String line=sc.nextLine();
+			System.out.println(findLargestWord(line));
 			break;
 		default:
 			System.out.println("Invalid choice");
