932cc9b53856bfc7aa8ac96791b65d0f12f1602a
meta-naveen-verma <79202706+meta-naveen-verma@users.noreply.github.com>
2021-03-09 10:08:32 +0530

    Update MainPoly.java

--- Abstract and Concrete Data Types/MainPoly.java
+++ Abstract and Concrete Data Types/MainPoly.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class MainPoly {
 
 	public static void main(String[] args) {
-		Poly p1 = new Poly(new double[]{2,3.5}, new int[]{2,1});
+		Poly p1 = new Poly(new double[]{2,3.5,0}, new int[]{2,1,5});
 		Poly p2 = new Poly(new double[]{3,5}, new int[]{2,1});
 		Poly add = Poly.addPoly(p1, p2);
 		Poly mul = Poly.mulPoly(p1, p2);
@@ -11,7 +11,7 @@ public class MainPoly {
 				+ Arrays.toString(add.getExponent()));
 		System.out.println(Arrays.toString(mul.getCoefficient())+"\n"
 				+ Arrays.toString(mul.getExponent()));
-			
+		System.out.println(p1.getDegree());
 
 	}
 
