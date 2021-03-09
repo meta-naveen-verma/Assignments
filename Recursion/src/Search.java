ac12f9369cbb674351198d8655dad87fac0777a6
meta-naveen-verma <79202706+meta-naveen-verma@users.noreply.github.com>
2021-03-09 09:08:58 +0530

    Update Search.java

--- Recursion/src/Search.java
+++ Recursion/src/Search.java
@@ -12,9 +12,10 @@ class Search
         return -1;
     }
   
- public static int binarysearch(int arr[], int x) 
+    public static int binarysearch(int arr[], int x) 
     { 
         int l = 0, r = arr.length - 1; 
+        //Arrays.sort(arr);
         while (l <= r) { 
             int m = l + (r - l) / 2; 
   
@@ -35,50 +36,36 @@ class Search
         return -1; 
     } 
  
-    public static void main(String args[])
-
-    {   Scanner sc=new Scanner(System.in);
-System.out.println("Press 1 for Linear search");
-System.out.println("Press 2 for Binary search");
-int ch=sc.nextInt();
-if(ch==1){
-
-        System.out.println("Enter size of array");
-int n=sc.nextInt();
-        int arr[] = new int[n];
- for(int i=0;i<n;i++){
-arr[i]=sc.nextInt();
-}
-System.out.println("Enter element to find");
-        int x = sc.nextInt();
- 
-       
-        int result = linearsearch(arr, x);
-        if (result == -1)
-            System.out.print(
-                "Element is not present in array");
-        else
-            System.out.print("Element is present at index "
-                             + result);}
-else{
-
-        System.out.println("Enter size of array");
-int n=sc.nextInt();
-        int arr[] = new int[n];
- for(int i=0;i<n;i++){
-arr[i]=sc.nextInt();
-}
-System.out.println("Enter element to find");
-        int x = sc.nextInt();
-	  int result1 = binarysearch(arr, x);
-        if (result1 == -1)
-            System.out.print(
-                "Element is not present in array");
-        else
-            System.out.print("Element is present at index "
-                             + result1);}
+    public static void main(String args[]){
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("Enter size of array");
+		int n=sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("Enter element to find");
+		int x = sc.nextInt();    
+		int result=-1;
+    	System.out.println("Press 1 for Linear search");
+    	System.out.println("Press 2 for Binary search");
+    	int ch=sc.nextInt();
+    	while(true){
+    		if(ch==1){
+    			result = linearsearch(arr, x);
+    			break;
+    		}
+    		else if (ch==2){
+    			result = binarysearch(arr, x);
+    			break;
+    		}
+    		else
+    			System.out.println("Make a right choise");
+    	}
+    	if (result == -1)
+			System.out.print("Element is not present in array");
+		else
+			System.out.print("Element is present at index "+ (result+1));
 
     }
-
-
 }
