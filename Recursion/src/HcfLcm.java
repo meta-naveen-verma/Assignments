2a51d125672938e3186a8a66bd6dae2e29469a85
meta-naveen-verma <79202706+meta-naveen-verma@users.noreply.github.com>
2021-03-09 09:06:40 +0530

    Update HcfLcm.java

--- Recursion/src/HcfLcm.java
+++ Recursion/src/HcfLcm.java
@@ -1,40 +1,40 @@
 import java.util.Scanner;
 class HcfLcm{
- public static int hcf(int n1, int n2)
+	public static int hcf(int num1, int num2)
     {
-        if (n2 != 0)
-            return hcf(n2, n1 % n2);
+        if (num2 != 0)
+            return hcf(num2, num1 % num2);
         else
-            return n1;
+            return num1;
     }
-public static void main(String[] args) {
-Scanner sc=new Scanner(System.in);
-	System.out.println("Enter the two numbers");
-        int n1 = sc.nextInt();
-        int n2 =  sc.nextInt();
-        int hcf = hcf(n1, n2);
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the two numbers");
+        int num1 = sc.nextInt();
+        int num2 =  sc.nextInt();
+        int hcf = hcf(num1, num2);
 
-        System.out.printf("G.C.D of %d and %d is %d.", n1, n2, hcf);
-	System.out.println();
+        System.out.printf("G.C.D of %d and %d is %d.", num1, num2, hcf);
+        System.out.println();
 
-	int lcm=lcmCalculation(n1,n2);
+        int lcm=lcm(num1,num2);
 
-        System.out.println("LCM of "+n1+" and "+n2+" is ="+lcm);
+        System.out.println("LCM of "+num1+" and "+num2+" is ="+lcm);
     }
- public static int lcmCalculation(int n1,int n2)
+	public static int lcm(int n1,int n2)
 	{
-	  int temp,i=2,res;
-	  if(n1>n2)
-		res=n1;
-	  else
-		res=n2;
-	   temp=res;
-	   while(res%n1!=0 || res%n2!=0)
+		int temp,i=2,res;
+		if(n1>n2)
+			res=n1;
+		else
+			res=n2;
+		temp=res;
+		while(res%n1!=0 || res%n2!=0)
 		{
-		res=temp*i;
-		i++;
+			res=temp*i;
+			i++;
 		}
-	  return res;
+		return res;
  
  	 }
 
