f0e86f3c85358eeac7cf1babad964e9e3f4dfb78
meta-naveen-verma <79202706+meta-naveen-verma@users.noreply.github.com>
2021-03-16 17:17:19 +0530

    Update Main.java

--- DSA A1/src/Main.java
+++ DSA A1/src/Main.java
@@ -23,11 +23,7 @@ public class Main {
 		System.out.println("After Operation");
 		lsOp.displayLinkList();
 		
-		System.out.println("Enter an equation---> ");
-		Scanner sc=new Scanner(System.in);
-		String str=sc.next();
-		MaxDegree md=new MaxDegree(str);
-		System.out.println("Max Degree is "+md.calculateMaxDegree());
+		
 	}
 
 }
