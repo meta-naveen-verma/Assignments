b2daa0af2d73c0d635fc5b2ee8199197c8d7d99f
meta-naveen-verma <79202706+meta-naveen-verma@users.noreply.github.com>
2021-03-26 10:25:27 +0530

    Update SortNatural.java

--- Objects and Collections in Programming Languages/src/SortNatural.java
+++ Objects and Collections in Programming Languages/src/SortNatural.java
@@ -1,7 +1,27 @@
+import java.util.Collections;
 import java.util.Comparator;
-public class SortNatural implements Comparator<Employee>{
-	public int compare(Employee a, Employee b){
-		return a.getId()-b.getId();
-	}
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+public class SortNatural{
+	public static HashMap<String,Employee> sortNatural(HashMap<String, Employee> hm)
+    {
+        List<Map.Entry<String, Employee> > list =
+               new LinkedList<Map.Entry<String, Employee> >(hm.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Employee> >() {
+            public int compare(Map.Entry<String, Employee> o1, 
+                               Map.Entry<String, Employee> o2)
+            {
+                return (o1.getValue().empId).compareToIgnoreCase(o2.getValue().empId);
+            }
+        });
+        HashMap<String, Employee> temp = new LinkedHashMap<String, Employee>();
+        for (Map.Entry<String, Employee> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
 
 }
